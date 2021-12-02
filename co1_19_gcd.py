bbac65aa2c686f32597b3e66214c49666a9e7c56
Raichal <raichalvarghese231@gmail.com>
2021-12-02 20:20:26 +0530

    second commit

--- co1_19_gcd.py
+++ co1_19_gcd.py
@@ -1,9 +1,15 @@
 # gcd of two numbers
 a=int(input("Enter the 1st no:"))
 b=int(input("Enter the 2nd no:"))
-def gcd():
+def gcd(a,b):
+    if a==0:
+        return a
+    if b==0:
+        return b
     if a==b:
-        print("gcd:",a)
-    else:
-        if a>b:
+        return a
+    if a>b:
+        return gcd(a-b,b)
+    return gcd(b-a,a)
+print("gcd(",a,",",b,") is",gcd(a,b))
             
---
+++ co1_7_sameLenSum.py
@@ -0,0 +1,14 @@
+lst1=[1,2,3,4,5,6]
+lst2=[1,8,9,11,10]
+if len(lst1)==len(lst2):
+    print("Same length")
+else:
+    print("Not same length")
+if sum(lst1)==sum(lst2):
+    print("Same sum")
+else:
+    print("Not same sum")
+
+        print("Same elements occur")
+else:
+        print("NO elements occur")
