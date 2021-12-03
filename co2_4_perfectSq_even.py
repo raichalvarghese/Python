a818dbaa20f0bf69c460ee17525cd08887ddf99d
raichalvarghese <53505130+raichalvarghese@users.noreply.github.com>
2021-12-03 11:51:16 +0530

    Update co2_4_perfectSq_even.py

--- co2_4_perfectSq_even.py
+++ co2_4_perfectSq_even.py
@@ -9,18 +9,24 @@ def isPerfect(x):
         p=math.sqrt(x)
         return ((p*p)==float(x))
     return false
+
 s=[]
 sm=0
-for i in range(num1,num2):
-    if isPerfect(i):
-        l=[]
-        while i>0:
-            rem=i%10
-            i=int(i/10)
-            l.append(rem)
-        if l[0]%2==0 and l[1]%2==0 and l[2]%2==0:
-            s.append(l)
 
+for i in range(num1,num2):
+        l=[]
+        num=i
+        while num>0:
+            rem=num%10
+            num=int(num/10)
+            l.append(rem)
+        if (l[0]%2==0) and (l[1]%2==0 and l[2]%2==0):
+            s.append(i)
+for x in s:
+    if (isPerfect(x)==True):
+        print(x)
+    else:
+        continue
         
         
 
