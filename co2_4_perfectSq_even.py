06ccb2fc7cfa50921b05ddc378578584a2ce0d50
Raichal <raichalvarghese231@gmail.com>
2021-12-07 06:59:43 +0530

    commit co2_4

--- co2_4_perfectSq_even.py
+++ co2_4_perfectSq_even.py
@@ -1,26 +1,29 @@
 #Generate a list of four digit numbers in a given range with all their digits even and the
 #number is a perfect square.
 
-import math
+from math import sqrt
 num1=int(input("Enter the first limit"))
 num2=int(input("Enter the final limit"))
 def isPerfect(x):
     if x>0:
-        p=math.sqrt(x)
-        return ((p*p)==float(x))
-    return false
-s=[]
-sm=0
-for i in range(num1,num2):
-    if isPerfect(i):
-        l=[]
-        while i>0:
-            rem=i%10
-            i=int(i/10)
-            l.append(rem)
-        if l[0]%2==0 and l[1]%2==0 and l[2]%2==0:
-            s.append(l)
+        p=int(sqrt(x))
+        return ((p*p)==x)
 
+s=[]
+for i in range(num1,num2):
+        l=[]
+        num=i
+        while num>0:
+            rem=num%10
+            num=int(num/10)
+            l.append(rem)
+        if (l[0]%2==0) and (l[1]%2==0 and l[2]%2==0 and l[3]%2==0):
+            s.append(i)
+for x in s:
+    if (isPerfect(x)):
+        print(x)
+    else:
+        continue
         
         
 
